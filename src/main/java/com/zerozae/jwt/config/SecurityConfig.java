2a0bd103dd70fdceafe82ca7b3f3b91ae3cc43d0
park0jae <okvv26@naver.com>
2023-06-14 14:25:58 +0900

    jwt Filter 등록 테스트

--- src/main/java/com/zerozae/jwt/config/SecurityConfig.java
+++ src/main/java/com/zerozae/jwt/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.zerozae.jwt.config;
 
+import com.zerozae.jwt.filter.MyFilter1;
+import com.zerozae.jwt.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.web.filter.CorsFilter;
 
@@ -20,11 +23,16 @@ public class SecurityConfig {
     private final CorsFilter corsFilter;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 시큐리티 필터 체인이 FilterConfig의 등록된 Filter들 보다 먼저 실행됨
+        http.addFilterBefore(new MyFilter3(), BasicAuthenticationFilter.class);
         http.csrf().disable()
+                        // 세션을 사용하지 않겠다 (웹은 기본으로 Stateless임 이를 stateful처럼 쓰기위해 세션이나 쿠키를 만드는데, 세션을 사용하지 않겠다고 선언한것과 똑같음)
                         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                         .and()
                         .addFilter(corsFilter) // @CrossOrigin(인증 없을때) , 시큐리티 필터에 인증 등록
+                        // 폼 로그인 안하겠다.
                         .formLogin().disable()
+                        //
                         .httpBasic().disable()
                         .authorizeRequests()
                         .antMatchers("/api/v1/user/**")
---
+++ src/main/java/com/zerozae/jwt/filter/MyFilter3.java
@@ -0,0 +1,17 @@
+package com.zerozae.jwt.filter;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+@Slf4j
+public class MyFilter3 implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("필터3");
+        chain.doFilter(request,response);
+    }
+}
